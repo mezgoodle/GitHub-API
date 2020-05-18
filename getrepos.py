32ee12e7243dd7f0d0ebc578932c7591fc62f6ef
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-05-18 21:44:48 +0300

    Update getrepos.py

--- getrepos.py
+++ getrepos.py
@@ -3,7 +3,7 @@ from getpass import getpass
 
 username = input('Enter your name: ')
 password = getpass()
-string = 'https://api.github.com/user'
+string = 'https://api.github.com/user/repos'
 
 r = requests.get(string, auth=(username, password))
-print(r.text)
+print(r.json())
