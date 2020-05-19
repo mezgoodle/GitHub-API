346d0b136a44a541c286117593b2e344a8197a89
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-05-19 12:33:34 +0300

    Delete debug prints

--- getfile.py
+++ getfile.py
@@ -9,8 +9,8 @@ repo = input('Repo: ')
 # GET /repos/:owner/:repo/readme
 # If you want other file
 file_path = 'README.md'
-print(username, repo)
+
 string = f'https://api.github.com/repos/{username}/{repo}/contents/{file_path}'
-print(string)
+
 file_response = requests.get(string)
 print(file_response.json())
