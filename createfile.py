152018ae5e906d6fa966413f831c4a23e646d032
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-05-20 20:28:09 +0300

    Update createfile.py

--- createfile.py
+++ createfile.py
@@ -8,4 +8,5 @@ password = getpass()
 content = 'Working GitHub API'
 b_content = content.encode('utf-8')
 base64_content = base64.b64encode(b_content)
-print(base64_content)
+base64_content_str = base64_content.decode('utf-8')
+print(base64_content_str)
