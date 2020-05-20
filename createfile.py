6262fb0d6605edc3b66069d15c64ace1119286ed
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-05-20 20:37:00 +0300

    Update createfile.py

--- createfile.py
+++ createfile.py
@@ -1,12 +1,26 @@
 import requests
+import json
 import base64
 from getpass import getpass
 
 username = input('Enter your name: ')
 password = getpass()
+repo = input('Repo: ')
+file_ = input('File: ')
 
 content = 'Working GitHub API'
 b_content = content.encode('utf-8')
 base64_content = base64.b64encode(b_content)
 base64_content_str = base64_content.decode('utf-8')
-print(base64_content_str)
+
+f = {
+    'path': '',
+    'message': 'Create new file via GitHub API',
+    'content': base64_content_str
+}
+
+f_resp = requests.put(f'https://api.github.com/repos/{username}/{repo}/contents/{file_}', 
+                        auth=(username, password), 
+                        headers={ "Content-Type": "application/json" }, 
+                        data=json.dumps(f))
+print(f_resp.json())
